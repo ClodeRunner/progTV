f8d0b9e496246d163342a9bfb72d890219383d3c
ClodeRunner <clode.runner@gmail.com>
2015-09-04 14:28:32 +0200

    Create progTV_Download.py

---
+++ progTV_Download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib
+import zipfile
+
+# script qui telecharge le fichier XML contenant les programmes TV
+# variables - les urls et noms pour download
+url = 'http://xmltv.dtdns.net/download/'
+nomDistant = 'complet.zip'
+urlnomDistant = url + nomDistant
+nomLocal = 'complet.zip'
+#dossierLocal = '/media/raid/'
+dossierLocal = '/users/nous/'
+dossierNomLocal = dossierLocal + nomLocal
+
+# actions - download puis unzip du fichier
+urllib.urlretrieve(urlnomDistant, dossierNomLocal)
+with zipfile.ZipFile('complet.zip', "r") as z:
+    z.extractall('./')
